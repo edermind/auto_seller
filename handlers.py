71a08bea6d9bb2d55cd9992e4e4dd1ba4cdac18e
edermind <kolesnikoffgrig@ya.ru>
2024-02-09 14:58:58 +0300

    4 general butons and st slots keyboard

--- handlers.py
+++ handlers.py
@@ -2,7 +2,7 @@ from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
 
-import keyboard
+import menu_keyboard
 import text
 
 router = Router()
@@ -10,20 +10,33 @@ router = Router()
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=keyboard.menu)
+    await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=menu_keyboard.menu)
 
-
-# @router.message(Command("my_slots"))
-# async def slots_command(msg: types.Message):
-#     await msg.answer('123')
-
+# Все что ниже это обработчики кнопок в главном меню(1)
 @router.callback_query(F.data == "my_slots")
 async def slots_command(callback_query: types.CallbackQuery):
     await callback_query.answer()
     await callback_query.message.answer(text.slots_test)
 
+@router.callback_query(F.data == "places")
+async def slots_command(callback_query: types.CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.answer(text.slots_test)
+
+@router.callback_query(F.data == "info")
+async def slots_command(callback_query: types.CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.answer(text.info)
+
+@router.callback_query(F.data == "supports")
+async def slots_command(callback_query: types.CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.answer(text.support)
+#вот до сюда(1)
+
+
 @router.message(F.text == "Меню")
 @router.message(F.text == "Выйти в меню")
 @router.message(F.text == "◀️ Выйти в меню")
 async def menu(msg: Message):
-    await msg.answer(text.menu, reply_markup=keyboard.menu)
+    await msg.answer(text.menu, reply_markup=menu_keyboard.menu)
--- menu_keyboard.py
+++ menu_keyboard.py
@@ -1,11 +1,14 @@
 
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+#кнопки главного меню
 menu = [
     [InlineKeyboardButton(text="🚗 Мои слоты", callback_data="my_slots"),
     InlineKeyboardButton(text="🏢 Площадки", callback_data="places")],
     [InlineKeyboardButton(text="💡Инфо", callback_data="info"),
     InlineKeyboardButton(text="🛟Техподдержка", callback_data="supports")],
 ]
+
 menu = InlineKeyboardMarkup(inline_keyboard=menu)
 exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
 iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
